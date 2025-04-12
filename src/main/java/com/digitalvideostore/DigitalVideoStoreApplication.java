4d25c406087347fe046e392063cde68b4b567bc8
teddy-adim <nwabuisiteddy@yahoo.com>
2025-04-11 22:16:40 -0400

    making changes to cors files

--- src/main/java/com/digitalvideostore/DigitalVideoStoreApplication.java
+++ src/main/java/com/digitalvideostore/DigitalVideoStoreApplication.java
@@ -2,8 +2,10 @@ package com.digitalvideostore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.digitalvideostore.repository")
 public class DigitalVideoStoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(DigitalVideoStoreApplication.class, args);
---
+++ src/main/java/com/digitalvideostore/config/MvcConfig.java
@@ -0,0 +1,44 @@
+package com.digitalvideostore.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.security.config.Customizer;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests((requests) -> requests
+                .anyRequest().permitAll()
+            )
+            .cors(Customizer.withDefaults())
+            .csrf(csrf -> csrf.disable())
+            .securityMatcher("/**");
+
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:3002", "https://movie-management-kappa.vercel.app"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+}
